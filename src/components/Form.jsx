57f19c3aa4c6f8957b65f3845655d1b1ce38c392
shehroz khan <sherozkhan321@gmail.com>
2023-08-09 21:07:56 +0500

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,147 @@
+import { useState } from "react";
+import GenForm from "./GenForm";
+
+function Form() {
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    age: "",
+    address: "",
+  });
+  const [forms, setForms] = useState([formData]);
+
+  const [secondData, setSecondData] = useState({
+    degree: "",
+    university: "",
+    location: "",
+  });
+
+  const [obj, setObj] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleDelete = (index) => {
+    const array = [...forms];
+    array.splice(index, 1);
+    setForms(array);
+  };
+
+  const handleForm = () => {
+    setForms([...forms, { degree: "", university: "", location: "" }]);
+  };
+
+  // console.log(forms);
+
+  const isValid = () => {
+    return (
+      formData.firstname.trim() !== "" &&
+      formData.lastname.trim() !== "" &&
+      formData.age !== "" &&
+      formData.address.trim() !== ""
+    );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isValid()) {
+      console.log("Form data submitted:", obj);
+      alert("Form submitted successfully");
+    } else {
+      console.log("Form data is incomplete");
+      alert("Form is incomplete");
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <h2>Contact Information</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="firstname">First Name:</label>
+          <input
+            type="text"
+            onChange={handleChange}
+            id="firstname"
+            name="firstname"
+            required
+          />
+          <p>
+            {formData.firstname && formData.firstname.length < 4
+              ? "Firstname should be greater than 4 chareacters"
+              : ""}
+          </p>
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastname">Last Name:</label>
+          <input
+            type="text"
+            id="lastname"
+            onChange={handleChange}
+            name="lastname"
+            required
+          />
+          <p>
+            {formData.lastname && formData.lastname.length < 4
+              ? "Lastname should be greater than 4 chareacters"
+              : ""}
+          </p>
+        </div>
+        <div className="form-group">
+          <label htmlFor="age">Age:</label>
+          <input
+            type="number"
+            onChange={handleChange}
+            id="age"
+            name="age"
+            required
+          />
+          <p>
+            {formData.age && formData.age > 15
+              ? "Age should be greater than 15"
+              : ""}
+          </p>
+        </div>
+        <div className="form-group">
+          <label htmlFor="address">Address:</label>
+          <textarea
+            id="address"
+            onChange={handleChange}
+            name="address"
+            rows="4"
+            required
+          />
+        </div>
+        <div>
+          <div className="form-group">
+            <button onClick={handleForm}>Add Education</button>
+          </div>
+          {forms.map((item, index) => {
+            if (index === 0) return null;
+
+            return (
+              <GenForm
+                key={index}
+                secondData={secondData}
+                setSecondData={setSecondData}
+                setObj={setObj}
+                formData={formData}
+                onClick={() => handleDelete(index)}
+              />
+            );
+          })}
+        </div>
+        <div className="form-group">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
---
+++ src/components/GenForm.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+function GenForm({ secondData, setSecondData, setObj, formData }) {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSecondData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    setObj({ ...formData, ...secondData });
+    // console.log(secondData);
+  };
+
+  return (
+    <div className="form-container">
+      <h2>Education Information</h2>
+      <div className="form-group">
+        <label>Degree:</label>
+        <input onChange={handleChange} name="degree" type="text" required />
+        <p>
+          {secondData.degree && secondData.degree.length >= 4
+            ? "Must be at least 4 characters"
+            : ""}
+        </p>
+      </div>
+      <div className="form-group">
+        <label>University:</label>
+        <input onChange={handleChange} name="university" type="text" required />
+      </div>
+      <div className="form-group">
+        <label>Location:</label>
+        <input onChange={handleChange} name="location" type="text" required />
+        <p>
+          {secondData.location && secondData.degree.location > 7
+            ? "Address should contain more detail "
+            : ""}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default GenForm;
